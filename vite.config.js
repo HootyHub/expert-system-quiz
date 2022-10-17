0462efabb7f3891924e79ac311295ee32da26484
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-10-17 17:31:10 +0200

    Vite and Docker setup

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: {
+      host: "localhost"
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [vue()]
+})
