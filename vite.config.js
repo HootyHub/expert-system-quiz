96769217cb09278519f663d264b500f85d0d09a3
Paulina Lencka <49527545+plencka@users.noreply.github.com>
2022-10-24 01:16:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
-import { defineConfig, loadEnv } from "vite"
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default ({ mode }) => {
-    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-    return defineConfig({
-      base: process.env.APP_BASE,
-      server: {
-        host: "0.0.0.0",
-        port: 5173,
-        hmr: {
-          host: "localhost"
-        },
-        watch: {
-          usePolling: true,
-        },
-      },
-      plugins: [vue()]
-  });
-};
+export default defineConfig({
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    hmr: {
+      host: "localhost"
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
+  plugins: [vue()]
+})
