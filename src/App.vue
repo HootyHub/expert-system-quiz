3ebc71f5063885f92d1a2bd0eaed09811cb5fcf4
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-10-24 12:33:31 +0200

    Glassmorphic design

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,29 @@
 <template>
-  <div class="flex items-center justify-center h-screen">
-    <div class="transition-all duration-300 w-5/6 md:w-2/3">
-      <div
-        class="border-4 border-gray-400 md:text-2xl p-3 rounded-lg flex items-center justify-center md:p-6 mb-3"
-      >
-        Czy papież lubi gumę turbo żuć?
-      </div>
-      <div class="min-h-full flex flex-col justify-center">
-        <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
-          <button
-            class="w-full bg-gray-200 rounded-lg p-4 transition md:text-2xl md:p-6 hover:bg-gray-300"
-          >
-            Tak
-          </button>
-          <button
-            class="w-full bg-gray-200 rounded-lg p-4 transition md:text-2xl md:p-6 hover:bg-gray-300"
-          >
-            Tak
-          </button>
+  <div
+    class="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"
+  >
+    <div
+      class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16"
+    >
+      <div class="">
+        <div
+          class="rounded-2xl font-semibold bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 text-lg md:text-2xl p-5 flex items-center justify-center md:p-6 mb-3"
+        >
+          Czy lubisz koty?
+        </div>
+        <div class="min-h-full flex flex-col justify-center">
+          <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
+            <button
+              class="w-full rounded-2xl bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 p-4 transition md:text-2xl md:p-6"
+            >
+              Tak
+            </button>
+            <button
+              class="w-full rounded-2xl bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 p-4 transition md:text-2xl md:p-6"
+            >
+              Tak
+            </button>
+          </div>
         </div>
       </div>
     </div>
