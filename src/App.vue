bda412d7cee3ffa80d3e2712dc5b763e8a491e2d
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-10-24 11:08:16 +0200

    Base design

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,25 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex items-center justify-center h-screen">
+    <div class="w-5/6 md:w-2/3">
+      <div
+        class="border-4 border-gray-400 md:text-2xl p-3 rounded-lg flex items-center justify-center md:p-6 mb-3"
+      >
+        Czy papież lubi gumę turbo żuć?
+      </div>
+      <div class="min-h-full flex flex-col justify-center">
+        <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
+          <button
+            class="w-full bg-gray-200 rounded-lg p-4 transition md:text-2xl md:p-6 hover:bg-gray-300"
+          >
+            Tak
+          </button>
+          <button
+            class="w-full bg-gray-200 rounded-lg p-4 transition md:text-2xl md:p-6 hover:bg-gray-300"
+          >
+            Tak
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
