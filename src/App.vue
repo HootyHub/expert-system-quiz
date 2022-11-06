e97a43890af96012e66c6b0e5b249656dc1ae597
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-11-06 16:44:04 +0100

    moving gradient background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"
+    class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-400 to-orange-500 via-purple-500 animate-gradient-x"
   >
     <div
       class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16"
