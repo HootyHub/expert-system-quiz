8ca64b8b7f38f7148783c9d83d822a11d386877d
sbanasiak <92038757+sbanasiak@users.noreply.github.com>
2022-11-07 17:16:26 +0100

    Prototype result view (#6)
    * prototype data
    
    * Updated data
    
    * More data changes
    
    * Orphan button center
    
    * Debug
    
    * Locale config
    
    Co-authored-by: Paulina Lencka <lencka.paulina@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,14 @@
+<script setup>
+  import ButtonAnswer from './components/ButtonAnswer.vue';
+  import DescriptionBlock from './components/DescriptionBlock.vue';
+  import Heading from './components/Heading.vue';
+  import Logo from './components/Logo.vue';
+
+  const props = defineProps({
+    });
+
+  const result = "python";
+</script>
 <template>
   <div
     class="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"
@@ -5,27 +16,26 @@
     <div
       class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16"
     >
-      <div class="">
-        <div
-          class="rounded-2xl font-semibold bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 text-lg md:text-2xl p-5 flex items-center justify-center md:p-6 mb-3"
-        >
-          Czy lubisz koty?
-        </div>
+      <section v-if="!result">
+        <Heading displayText="Czy lubisz koty?"/>
         <div class="min-h-full flex flex-col justify-center">
-          <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-2">
-            <button
-              class="w-full rounded-2xl bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 p-4 transition md:text-2xl md:p-6"
-            >
-              Tak
-            </button>
-            <button
-              class="w-full rounded-2xl bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 p-4 transition md:text-2xl md:p-6"
-            >
-              Tak
-            </button>
+          <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
+            <ButtonAnswer displayText="Tak"/>
+            <ButtonAnswer displayText="Nie"/>
+            <ButtonAnswer displayText="Nie wiem"/>
           </div>
         </div>
-      </div>
+      </section>
+      <section v-else>
+        <Logo logoName="python"/>
+        <Heading displayText="Python"/>
+        <DescriptionBlock displayText="Lorem ipsum"/>
+        <div class="min-h-full flex flex-col justify-center">
+          <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
+            <ButtonAnswer displayText="Restart"/>
+          </div>
+        </div>
+      </section>
     </div>
   </div>
 </template>
---
+++ src/components/Button.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const emit = defineEmits(["click"]);
+    const props = defineProps({
+        displayText: String,
+    });
+</script>
+<template>
+    <button
+        @click="$emit('click')"
+        class="w-full rounded-2xl bg-white shadow-lg backdrop-filter backdrop-blur-lg bg-opacity-50 p-4 transition md:text-2xl md:p-6"
+    >
+        {{ props.displayText }}
+    </button>
+</template>
