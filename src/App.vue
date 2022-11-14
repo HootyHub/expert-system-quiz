6f786fb6c14e872d8b7a427abee43dbf97d2f82d
Paulina Lencka <lencka.paulina@gmail.com>
2022-11-15 00:00:53 +0100

    Fixed routes and paths for build

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,5 @@
 <script setup>
-import { ref, computed } from 'vue'
-import Start from './pages/Start.vue'
-import Quiz from './pages/Quiz.vue'
-import Result from './pages/Result.vue'
-import NotFound from './pages/NotFound.vue'
 
-const routes = {
-  '/': Start,
-  '/quiz': Quiz,
-  '/result': Result
-}
-
-const currentPath = ref(window.location.hash)
-
-window.addEventListener('hashchange', () => {
-  currentPath.value = window.location.hash
-})
-
-const currentView = computed(() => {
-  return routes[currentPath.value.slice(1) || '/'] || NotFound
-})
 </script>
 
 <template>
@@ -29,7 +9,7 @@ const currentView = computed(() => {
     <div
       class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16"
     >
-      <component :is="currentView" />
+      <router-view/>
     </div>
   </div>
 </template>
--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,10 +1,10 @@
 <script setup>
-    import ButtonAnswer from '../components/ButtonAnswer.vue';
+    import ButtonAnswer from '@/components/ButtonAnswer.vue';
 </script>
 <template>
     <div class="min-h-full flex flex-col justify-center">
-        <a href="/#/quiz" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
+        <router-link to="/quiz" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
             <ButtonAnswer displayText="Zacznij quiz"/>
-        </a>
+        </router-link>
     </div>
 </template>
