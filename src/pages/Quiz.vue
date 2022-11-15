ce73ee619715ab4b0deb777c3d901e9235ebd401
sbanasiak <seweryn.banasiak@studenci.collegiumwitelona.pl>
2022-11-15 21:30:38 +0100

    score simplified

--- src/pages/Quiz.vue
+++ src/pages/Quiz.vue
@@ -1,108 +1,109 @@
 <script setup>
-  import { onMounted, ref } from "vue";
-  import ButtonAnswer from '@/components/ButtonAnswer.vue';
-  import Heading from '@/components/Heading.vue';
-  import data from '@/assets/data.yml'
+import { onMounted, ref } from "vue";
+import ButtonAnswer from "@/components/ButtonAnswer.vue";
+import Heading from "@/components/Heading.vue";
+import data from "@/assets/data.yml";
 
-  const loading = ref(true);
-  const genericQuestions = data.generic_questions;
-  const currentQuestion = ref(null);
-  const categories = ref(null);
+const loading = ref(true);
+const genericQuestions = data.generic_questions;
+const currentQuestion = ref(null);
+const categories = ref(null);
 
-  function getGenericQuestion(){
-    var index = Math.floor(Math.random() * genericQuestions.length);
-    currentQuestion.value = genericQuestions[index];
-    genericQuestions.splice(index, 1);
+function getGenericQuestion() {
+  var index = Math.floor(Math.random() * genericQuestions.length);
+  currentQuestion.value = genericQuestions[index];
+  genericQuestions.splice(index, 1);
+}
+
+function getQuestionFromBestCategory() {
+  var bestCategory = Object.keys(categories.value).reduce(function (a, b) {
+    return categories.value[a].score > categories.value[b].score ? a : b;
+  });
+  if (categories.value[bestCategory].questions.length <= 0) {
+    alert("Best: " + bestCategory);
+    return;
   }
-
-  function getQuestionFromBestCategory(){
-    var bestCategory = Object.keys(categories.value).reduce(function(a, b){ return categories.value[a].score > categories.value[b].score ? a : b });
-    if(categories.value[bestCategory].questions.length <= 0){
-      alert("Best: " + bestCategory);
-      return;
-    }
-    var index = Math.floor(Math.random() * categories.value[bestCategory].questions.length);
-    currentQuestion.value = categories.value[bestCategory].questions[index];
-    for (var key in currentQuestion.value.answers) {
-      var tiers = ["positive", "negative", "neutral"];
-        for(var i in tiers){
-          if(currentQuestion.value.answers[key].result === tiers[i]){
-            currentQuestion.value.answers[key].result = {};
-            currentQuestion.value.answers[key].result[tiers[i]] = [bestCategory];
-          }
-        }
+  var index = Math.floor(
+    Math.random() * categories.value[bestCategory].questions.length
+  );
+  currentQuestion.value = categories.value[bestCategory].questions[index];
+  for (var key in currentQuestion.value.answers) {
+    var tiers = ["positive", "negative", "neutral"];
+    for (var i in tiers) {
+      if (currentQuestion.value.answers[key].result === tiers[i]) {
+        currentQuestion.value.answers[key].result = {};
+        currentQuestion.value.answers[key].result[tiers[i]] = [bestCategory];
       }
-    categories.value[bestCategory].questions.splice(index, 1);
-  }
-
-  function getQuestion(){
-    loading.value = true;
-    if(genericQuestions.length > 0 ){
-        getGenericQuestion();
-      }
-      else{
-        getQuestionFromBestCategory();
-    }
-    setTimeout(function() {
-      loading.value = false;
-    }, 500);
-  }
-
-  function loadCategories(){
-    categories.value = data.categories;
-    for (var key in categories.value) {
-        categories.value[key].score = 0;
     }
   }
+  categories.value[bestCategory].questions.splice(index, 1);
+}
 
-  function beginQuiz(){
-    loadCategories();
-    getQuestion();
+function getQuestion() {
+  loading.value = true;
+  if (genericQuestions.length > 0) {
+    getGenericQuestion();
+  } else {
+    getQuestionFromBestCategory();
   }
+  setTimeout(function () {
+    loading.value = false;
+  }, 500);
+}
 
-  function score(answer){
-    if(answer.result.positive){
-        for (var key in answer.result.positive) {
-            categories.value[answer.result.positive[key]].score += 1;
-        }
-    }
-    if(answer.result.negative){
-        for (var key in answer.result.negative) {
-            categories.value[answer.result.negative[key]].score -= 2;
-        }
-    }
-    if(answer.result.neutral){
-        for (var key in answer.result.neutral) {
-            categories.value[answer.result.neutral[key]].score += 0;
-        }
+function loadCategories() {
+  categories.value = data.categories;
+  for (var key in categories.value) {
+    categories.value[key].score = 0;
+  }
+}
+
+function beginQuiz() {
+  loadCategories();
+  getQuestion();
+}
+
+function score(answer) {
+  const results = { positive: 1, negative: -2, neutral: 0 };
+
+  for (var key in answer.result) {
+    for (var i in answer.result[key]) {
+      categories.value[answer.result[key][i]].score += results[key];
     }
   }
+}
 
-  function handleAnswer(answer){
-    getQuestion();
-    score(answer);
-  }
+function handleAnswer(answer) {
+  getQuestion();
+  score(answer);
+}
 
-  onMounted(() => {
-    beginQuiz();
+onMounted(() => {
+  beginQuiz();
 });
 </script>
 <template>
-    <template v-if="!loading && currentQuestion">
-        <Heading :displayText="currentQuestion.query"/>
-          <div class="min-h-full flex flex-col justify-center">
-            <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
-              <template v-for="answer in currentQuestion.answers">
-                <ButtonAnswer :displayText="answer.value" @click="handleAnswer(answer)"/>
-              </template>
-            </div>
-          </div>
+  <template v-if="!loading && currentQuestion">
+    <Heading :displayText="currentQuestion.query" />
+    <div class="min-h-full flex flex-col justify-center">
+      <div class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
+        <template v-for="answer in currentQuestion.answers">
+          <ButtonAnswer
+            :displayText="answer.value"
+            @click="handleAnswer(answer)"
+          />
         </template>
-        <template v-else>
-          <div class="flex items-center justify-center">
-            <div class="spinner-border animate-spin inline-block w-20 h-20 border-8 rounded-full text-red-300" role="status">
-              <span class="visually-hidden">Loading...</span>
-            </div>
-        </div>
-    </template> 
+      </div>
+    </div>
+  </template>
+  <template v-else>
+    <div class="flex items-center justify-center">
+      <div
+        class="spinner-border animate-spin inline-block w-20 h-20 border-8 rounded-full text-red-300"
+        role="status"
+      >
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  </template>
 </template>
