b1b3bb6775c29b502d0fc363337e5aa69a9a280f
Paulina Lencka <49527545+plencka@users.noreply.github.com>
2022-11-16 00:36:33 +0100

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Start from '@/pages/Start.vue'
 import Quiz from '@/pages/Quiz.vue'
 import Result from '@/pages/Result.vue'
@@ -6,29 +6,29 @@ import NotFound from '@/pages/NotFound.vue'
 
 const routes = [
     {
-        path: import.meta.env.BASE_URL,
+        path: '/',
         name: 'Start',
         component: Start,
     },
     {
-        path: import.meta.env.BASE_URL + 'quiz',
+        path: '/quiz',
         name: 'Quiz',
         component: Quiz,
     },
     {
-        path: import.meta.env.BASE_URL + 'result/:key',
+        path: '/result/:key',
         name: 'Result',
         component: Result,
         props: true
     },
     {
-        path: import.meta.env.BASE_URL + ':pathMatch(.*)*',
+        path: '/:pathMatch(.*)*',
         name: 'NotFound',
         component: NotFound,
     },
 ]
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes,
 })
 export default router
