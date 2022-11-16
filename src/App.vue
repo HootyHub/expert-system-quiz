775c35cfde16d735b8565a7838f7f5eafa029375
Paulina Lencka <49527545+plencka@users.noreply.github.com>
2022-11-16 10:27:18 +0100

    Language names and stuff (#10)
    * New config variables
    
    * Some adjustments

--- src/App.vue
+++ src/App.vue
@@ -4,10 +4,10 @@
 
 <template>
   <div
-    class="flex items-center justify-center h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100"
+    class="flex items-center justify-center h-screen"
   >
     <div
-      class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16"
+      class="bg-white backdrop-filter backdrop-blur-lg bg-opacity-20 rounded-xl shadow-lg transition-all duration-300 w-5/6 md:w-2/3 p-4 md:p-16 font-mono"
     >
       <router-view/>
     </div>
--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -1,9 +1,12 @@
 <script setup>
     import ButtonAnswer from '@/components/ButtonAnswer.vue';
+    import Heading from '@/components/Heading.vue';
 </script>
 <template>
-    <div class="min-h-full flex flex-col justify-center">
-        <router-link to="/quiz" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
+    <div class="min-h-full flex flex-col justify-center text-center">
+        <Heading displayText="Quiz - który język programowania wybrać?"/>
+        <span class="text-gray-800 font-semibold">Autorzy: Paulina Lencka, Seweryn Banasiak</span>
+        <router-link to="/quiz" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4 mt-2">
             <ButtonAnswer displayText="Zacznij quiz"/>
         </router-link>
     </div>
