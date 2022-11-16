279ed462f01c847acb3b80dc41123107c6d31ecd
Paulina Lencka <lencka.paulina@gmail.com>
2022-11-16 10:25:42 +0100

    Some adjustments

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -1,9 +1,9 @@
 <script setup>
     import { computed } from 'vue'
     import ButtonAnswer from '@/components/ButtonAnswer.vue';
-    import DescriptionBlock from '@/components/DescriptionBlock.vue';
     import Heading from '@/components/Heading.vue';
     import Logo from '@/components/Logo.vue';
+    import data from "@/assets/data.yml";
     import { useRoute } from 'vue-router'
 
     const route = useRoute()
@@ -14,8 +14,7 @@
 </script>
 <template>
     <Logo :logoName="result"/>
-    <Heading :displayText="result"/>
-    <DescriptionBlock :displayText="result + '_description'"/>
+    <Heading :displayText="data.languages.find(language => language.key === result).name"/>
     <div class="min-h-full flex flex-col justify-center">
         <router-link to="/" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4">
             <ButtonAnswer displayText="Strona główna"/>
