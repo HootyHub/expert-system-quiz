111417da8a84b63cad783f672dd36547149de9ed
Paulina Lencka <49527545+plencka@users.noreply.github.com>
2022-11-16 10:49:10 +0100

    bg fix attempt (#11)

--- src/pages/Start.vue
+++ src/pages/Start.vue
@@ -5,7 +5,7 @@
 <template>
     <div class="min-h-full flex flex-col justify-center text-center">
         <Heading displayText="Quiz - który język programowania wybrać?"/>
-        <span class="text-gray-800 font-semibold">Autorzy: Paulina Lencka, Seweryn Banasiak</span>
+        <span class="text-gray-200 font-light">Autorzy: Paulina Lencka, Seweryn Banasiak</span>
         <router-link to="/quiz" class="grid grid-cols-1 gap-4 md:gap-4 md:grid-cols-4 mt-2">
             <ButtonAnswer displayText="Zacznij quiz"/>
         </router-link>
