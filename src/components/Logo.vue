9795899c05210f82a4d9e8a4ebd86501f7007fcb
Paulina Lencka <49527545+plencka@users.noreply.github.com>
2022-11-16 01:08:50 +0100

    Update Logo.vue

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 <template>
   <div class="p-5 flex items-center justify-center md:p-6 mb-3">
     <img
-      :src="'/logos/' + props.logoName + '.svg'"
+      :src="'logos/' + props.logoName + '.svg'"
       class="drop-shadow-lg h-16 w-16 md:h-48 transition-all md:w-48"
     />
   </div>
